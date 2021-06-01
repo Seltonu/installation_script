847a50b94758375bffb4a9bbdec7b568ced2d7cd
Steven Gutierrez <steven.s.gutier@gmail.com>
2021-06-01 06:32:53 -0700

    Misc. changes, whole script no longer should be run as sudo

--- installation_script.sh
+++ installation_script.sh
@@ -8,23 +8,32 @@ cd
 clear
 sudo apt update && sudo apt upgrade -y && flatpak update -y
 
-################## install software
+################## install software ################## 
+sudo apt install -y lutris steam flameshot
+
 # "sudo -u $SUDO_USER" is needed to run the commands outside of sudo (normal user), required for flatpak installation
-sudo -u $SUDO_USER flatpak install flathub com.discordapp.Discord -y
-sudo -u $SUDO_USER flatpak install flathub com.spotify.Client -y
-sudo -u $SUDO_USER flatpak install flathub com.visualstudio.code -y
-sudo -u $SUDO_USER flatpak install flathub org.deluge_torrent.deluge -y
-sudo -u $SUDO_USER flatpak install flathub org.kde.kdenlive -y
-sudo -u $SUDO_USER flatpak install flathub org.kde.krita -y
-sudo -u $SUDO_USER flatpak install flathub org.gnome.Boxes -y
-sudo -u $SUDO_USER flatpak install flathub us.zoom.Zoom -y
-sudo -u $SUDO_USER flatpak install flathub org.videolan.VLC -y
-sudo -u $SUDO_USER flatpak install flathub com.obsproject.Studio -y
-sudo -u $SUDO_USER flatpak install flathub com.mojang.Minecraft -y
-sudo -u $SUDO_USER flatpak install flathub org.darktable.Darktable -y
-sudo -u $SUDO_USER flatpak install flathub com.rawtherapee.RawTherapee -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub com.visualstudio.code -y
+flatpak install flathub org.deluge_torrent.deluge -y
+flatpak install flathub org.kde.kdenlive -y
+flatpak install flathub org.kde.krita -y
+flatpak install flathub org.gnome.Boxes -y
+flatpak install flathub us.zoom.Zoom -y
+flatpak install flathub org.videolan.VLC -y
+flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub com.mojang.Minecraft -y
+flatpak install flathub org.darktable.Darktable -y
+flatpak install flathub com.rawtherapee.RawTherapee -y
 
 
+#add pip modules
+sudo apt install python3-pip -y
+pip_packages="discord.py[voice]"
+python3 -m pip install -U "$pip_packages"
+
+
+################## customizations ################## 
 # WARNING: this will overwrite any existing .bash_aliases file!
 echo "alias upup='sudo apt update && sudo apt upgrade -y && flatpak update -y'" > .bash_aliases
 echo "alias minecraft='clear && cd ~/Minecraft\ Server/ && ./start.sh'" >> .bash_aliases
@@ -34,22 +43,22 @@ source .bash_aliases #load aliases file to be used immediately
 
 #Change device name
 device_name="box"
-#echo $device_name > /etc/hostname
-#python3 rename_files.py  #this needs to be modified to accept command line args first
-#sudo sed "s/pop-os/$device_name/" /etc/hosts
+sudo hostnamectl set-hostname --static $device_name
+# echo $device_name > /etc/hostname
+# sudo sed "s/pop-os/$device_name/" /etc/hosts
 
-
-#add pip modules
-sudo apt install python3-pip -y
-pip_packages="discord.py[voice]"
-python3 -m pip install -U "$pip_packages"
+gsettings set org.gnome.desktop.session idle-delay 720 #set screen off time to 12 minutes
+# ACTION="flameshot gui"
+# KEY="PrintSc"
+# gsettings set org.gnome.desktop.wm.keybindings $KEY $ACTION
+# gsettings set org.gnome.mutter.keybindings $KEY $ACTION
+# gsettings set org.gnome.settings-daemon.plugins.media-keys $KEY $ACTION
 
 
 #Notes:
 #input is taken with "read foo"
 
 #Todo:
-#install flatpak softwares I use
 #change mount points of attached drives to identify by label
 #create symlinks for folders on Koi drive to home
 #set refresh rates of monitors to highest
