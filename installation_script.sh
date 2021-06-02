7d868f5b1dfa8415587d1f08e8d289e8988b0e37
Steven Gutierrez <steven.s.gutier@gmail.com>
2021-06-02 02:28:57 -0700

    Added Japanese keyboard

--- installation_script.sh
+++ installation_script.sh
@@ -8,28 +8,32 @@ clear
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && flatpak update -y
 
 # ################## install software ################## 
-# sudo apt install -y lutris steam flameshot
+sudo apt install -y lutris steam flameshot
+flameshot config --autostart true --trayicon false --maincolor \#4287f5
 
 # # "sudo -u $SUDO_USER" is needed to run the commands outside of sudo (normal user), required for flatpak installation
-# flatpak install flathub com.discordapp.Discord -y
-# flatpak install flathub com.spotify.Client -y
-# flatpak install flathub com.visualstudio.code -y
-# flatpak install flathub org.deluge_torrent.deluge -y
-# flatpak install flathub org.kde.kdenlive -y
-# flatpak install flathub org.kde.krita -y
-# flatpak install flathub org.gnome.Boxes -y
-# flatpak install flathub us.zoom.Zoom -y
-# flatpak install flathub org.videolan.VLC -y
-# flatpak install flathub com.obsproject.Studio -y
-# flatpak install flathub com.mojang.Minecraft -y
-# flatpak install flathub org.darktable.Darktable -y
-# flatpak install flathub com.rawtherapee.RawTherapee -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub com.visualstudio.code -y
+flatpak install flathub org.deluge_torrent.deluge -y
+flatpak install flathub org.kde.kdenlive -y
+flatpak install flathub org.kde.krita -y
+flatpak install flathub org.gnome.Boxes -y
+flatpak install flathub us.zoom.Zoom -y
+flatpak install flathub org.videolan.VLC -y
+flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub com.mojang.Minecraft -y
+flatpak install flathub org.darktable.Darktable -y
+flatpak install flathub com.rawtherapee.RawTherapee -y
 
 
-# #add pip modules
-# sudo apt install python3-pip -y
-# pip_packages='discord.py[voice] youtube-dl'
-# sudo python3 -m pip install -U $pip_packages
+#add pip modules
+sudo apt install python3-pip -y
+pip_packages='discord.py[voice] youtube-dl'
+sudo python3 -m pip install -U $pip_packages
+
+
+
 
 
 ################## customizations ################## 
@@ -45,21 +49,20 @@ source .bash_aliases #load aliases file to be used immediately
 #Change device name
 device_name="box"
 sudo hostnamectl set-hostname --static $device_name
-# echo $device_name > /etc/hostname
-# sudo sed "s/pop-os/$device_name/" /etc/hosts
 
+printf "Updating gsettings"
 gsettings set org.gnome.desktop.session idle-delay 720 #set screen off time to 12 minutes
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface clock-show-seconds true
 gsettings set org.gnome.desktop.calendar show-weekdate true
-# ACTION="flameshot gui"
-# KEY="PrintSc"
-# gsettings set org.gnome.desktop.wm.keybindings $KEY $ACTION
-# gsettings set org.gnome.mutter.keybindings $KEY $ACTION
-# gsettings set org.gnome.settings-daemon.plugins.media-keys $KEY $ACTION
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'mozc-jp')]" #add Japanese keyboard
+
 
 git config --global user.name "Steven Gutierrez"
 git config --global user.email "steven.s.gutier@gmail.com"
+
+
+printf "\nSettings updated.\n"
 
 #Notes:
 #input is taken with "read foo"
